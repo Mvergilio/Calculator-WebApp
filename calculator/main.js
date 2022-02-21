532e5d9d14b012d9df73eb98221d6be07ac384a1
Mvergilio <mvergiliodesouza@gmail.com>
2022-02-21 11:47:19 -0300

    troubleshooting

--- calculator/main.js
+++ calculator/main.js
@@ -1,66 +1,78 @@
-class Calculator{
-    constructor(currentOperandTextElement, previuosOperandTextElement){
+class Calculator {
+    constructor(currentOperandTextElement, previuosOperandTextElement) {
         this.currentOperandTextElement = currentOperandTextElement;
         this.previuosOperandTextElement = previuosOperandTextElement;
         this.clearAll()
         // this.operation = operation;
     }
-    clearAll(){
+    clearAll() {
         this.currentOperand = "";
         this.previuosOperand = "";
         this.operation = undefined;
     }
-    delete(){
-        
-    }
-    appendNumber(number){
-        if(number === "." && this.currentOperand.includes("."))return;
-
-            this.currentOperand = this.currentOperand.toString() + number.toString();
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
 
     }
-    chooseOperation(operation){
-    this.operation = operation;
-    
-    if(this.currentOperand.length == 0)return;
-this.currentOperand = this.currentOperand.toString() + this.operation.toString();
-this.previuosOperand = this.currentOperand;
-this.currentOperand = "";
+    appendNumber(number) {
+        if (number === "." && String(this.currentOperand).includes(".")) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
-    compute(){
-this.value = this.previuosOperandTextElement.innerText.slice(-1);
-this.currentOperand = Number(this.currentOperand);
-this.previuosOperand = Number(this.previuosOperandTextElement.innerText.slice(0,-1));
-switch (this.value) {
-    case "÷":
-        this.currentOperand = this.previuosOperand / this.currentOperand;
-        break;
-        
-        case "+":
-        this.currentOperand = this.previuosOperand + this.currentOperand;
 
-        break;
 
-    case "-":
-        this.currentOperand = this.previuosOperand - this.currentOperand;
 
-        break;
-        
-        case "*":
-            this.currentOperand = this.previuosOperand * this.currentOperand;
-            
-            break;
+    chooseOperation(operation) {
 
-            default:
+        if (this.currentOperand !== '') return;
+        if (this.previuosOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previuosOperand = this.currentOperand;
+        this.currentOperand = "";
+    }
+
+
+
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previuosOperand);
+        const current = parseFloat(this.currentOperand);
+        console.log(this.operation)
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case "÷":
+                computation = prev / current;
                 break;
-    }
-    this.previuosOperand = "";
-}
 
-updateDisplay(){
-    this.currentOperandTextElement.innerText = this.currentOperand;
-    this.previuosOperandTextElement.innerText = this.previuosOperand;
-}
+            case "+":
+                computation = prev + current;
+
+                break;
+
+            case "-":
+                computation = prev - current;
+
+                break;
+
+            case "*":
+                computation = prev * current;
+                break;
+            default:
+                return;
+
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previuosOperand = "";
+
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previuosOperandTextElement.innerText = this.previuosOperand;
+    }
 }
 const PREVIUOS_OPERAND = document.querySelector('[data-privious]');
 const CURRENT_OPERAND = document.querySelector('[data-current]');
@@ -72,25 +84,31 @@ const EQUALS_BTN = document.querySelector('[data-equals]');
 // console.log(EQUALS_BTN)
 const CALCULATOR = new Calculator(CURRENT_OPERAND, PREVIUOS_OPERAND)
 NUMBERS.forEach(element => {
-    element.addEventListener("click", ()=>{
+    element.addEventListener("click", () => {
         CALCULATOR.appendNumber(element.innerText)
         CALCULATOR.updateDisplay();
     })
-    
+
 });
-CLEAR_ALL.addEventListener("click",()=>{
+
+CLEAR_ALL.addEventListener("click", () => {
     CALCULATOR.clearAll();
+    CALCULATOR.updateDisplay();
+})
+DELETE_OPERAND.addEventListener("click", () => {
+    CALCULATOR.delete();
     CALCULATOR.updateDisplay();
 })
 
 OPERATION_BRNS.forEach(element => {
-    element.addEventListener("click", ()=>{
+    element.addEventListener("click", () => {
         CALCULATOR.chooseOperation(element.innerText)
+        console.log(element.innerText)
         CALCULATOR.updateDisplay();
     })
-    
+
 });
-EQUALS_BTN.addEventListener("click", ()=>{
-        CALCULATOR.compute()
-        CALCULATOR.updateDisplay();
-    })
+EQUALS_BTN.addEventListener("click", () => {
+    CALCULATOR.compute()
+    CALCULATOR.updateDisplay();
+})
